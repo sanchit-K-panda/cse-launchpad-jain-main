6b35ee89a626783f09aeaaf313efdea0c994e924
Sanchit <sleeppypanda0@gmail.com>
2026-02-18 18:09:08 +0530

    commit

--- cse-launchpad-jain-main/src/lib/db.ts
+++ cse-launchpad-jain-main/src/lib/db.ts
@@ -1,7 +1,9 @@
 import { neon } from '@neondatabase/serverless';
 
-if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL is not defined in environment variables');
+
+
+if (!import.meta.env.VITE_DATABASE_URL) {
+    throw new Error('VITE_DATABASE_URL is not defined in environment variables');
 }
 
-export const sql = neon(process.env.DATABASE_URL);
+export const sql = neon(import.meta.env.VITE_DATABASE_URL);
