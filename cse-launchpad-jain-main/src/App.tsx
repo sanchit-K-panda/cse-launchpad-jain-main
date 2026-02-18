b15bd46226e8f265b943d2850798446e74976b03
Sanchit <sleeppypanda0@gmail.com>
2026-02-18 11:05:55 +0530

    ..

--- cse-launchpad-jain-main/src/App.tsx
+++ cse-launchpad-jain-main/src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Events from "./pages/Events";
@@ -61,3 +61,4 @@ const App = () => (
 );
 
 export default App;
+
